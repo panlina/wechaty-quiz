08de52e7a875b82a094e3a0a3e197c2e3063c1de
Jack Lu <jacklu@jacklu.me>
2022-03-07 13:51:32 +0800

    +WechatyQuizPlugin

---
+++ index.js
@@ -0,0 +1,66 @@
+/** @typedef { import("wechaty").Wechaty } Wechaty */
+/** @typedef { import("wechaty").Room } Room */
+/** @typedef { import("wechaty").Message } Message */
+var { MessageType } = require("wechaty-puppet");
+
+/**
+ * @param {Object} config
+ * @param {() => Promise<{ question: string, answer: string }>} config.fetch quiz fetcher
+ * @param {number} config.voteMin min vote to start the quiz
+ * @param {number} config.voteTimeout vote timeout in milliseconds
+ * @param {number} config.answerTimeout answer timeout in milliseconds
+ */
+module.exports = function WechatyQuizPlugin(config) {
+	return function (/** @type {Wechaty} */bot) {
+		bot.on("message", async (/** @type {Message} */message) => {
+			if (message.talker().self()) {
+				var match = message.text().match(/^quiz: (.*)/);
+				if (match) {
+					var [, roomTopic] = match;
+					var room = await bot.Room.find({ topic: roomTopic });
+					if (!room) {
+						bot.userSelf().say(`Room ${roomTopic} not found`);
+						return;
+					}
+					quiz(room);
+				}
+			}
+		});
+	};
+	async function quiz(/** @type {Room} */room) {
+		var vote = 0;
+		await room.say(`抢答比赛即将进行，${config.voteTimeout / 1000}秒内回复\".\"人数>=${config.voteMin}时比赛开始。比赛奖励：红包5元。`);
+		room.on('message', voteCounter);
+		function voteCounter(/** @type {Message} */message) {
+			if (message.text() == '.') vote++;
+		}
+		setTimeout(async () => {
+			room.off('message', voteCounter);
+			if (vote >= config.voteMin) {
+				await room.say(`人数足够，即将开始比赛，请作好准备，抢答时间只有${config.answerTimeout / 1000}秒。`);
+				setTimeout(async () => {
+					var quiz = await config.fetch();
+					await room.say(quiz.question);
+					room.on('message', answerListener);
+					var timer = setTimeout(async () => {
+						room.off('message', answerListener);
+						await room.say("时间到，没有人答对，本次比赛结束。");
+						await room.say(`我们下次比赛再见。`);
+					}, config.answerTimeout);
+					async function answerListener(/** @type {Message} */message) {
+						if (message.type() == MessageType.Unknown) return;
+						if (message.text() == quiz.answer) {
+							room.off('message', answerListener);
+							clearTimeout(timer);
+							await room.say("回答正确。");
+							await room.say(`恭喜${message.talker().name()}赢得了本次比赛。`);
+							await room.say(`我们下次比赛再见。`);
+						} else
+							await message.say("回答错误。");
+					}
+				}, 5 * 1000);
+			} else
+				await room.say("人数不足，我们下次比赛再见。");
+		}, config.voteTimeout);
+	}
+};
