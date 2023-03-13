7a407e74546593dee31c854d7d06463972442cb0
Jack Lu <jacklu@jacklu.me>
2023-03-13 22:45:23 +0800

    test: +filter

--- test/test.js
+++ test/test.js
@@ -62,6 +62,42 @@ it('', async function() {
 	await bot.stop();
 });
 
+it('filter', async function() {
+	this.timeout(10000);
+
+	var mocker = new Mocker();
+
+	var puppet = new PuppetMock({ mocker });
+	var bot = new Wechaty({ puppet });
+	bot.use(new WechatyQuizPlugin({
+		filter: [{ topic: /t/ }],
+		fetch: () => Promise.resolve({ question: "a", answer: "b" }),
+		voteMin: 2,
+		voteTimeout: 5 * 1000,
+		answerTimeout: 10 * 1000
+	}));
+
+	await bot.start();
+
+	mocker.scan('https://github.com/wechaty', 1);
+	var user = mocker.createContact();
+	mocker.login(user);
+
+	var contactA = mocker.createContact({ id: 'a' });
+	var contactB = mocker.createContact({ id: 'b' });
+	var roomA = mocker.createRoom({ topic: 't', memberIdList: [user.id, contactA.id, contactB.id] });
+	var roomB = mocker.createRoom({ topic: 's', memberIdList: [user.id, contactA.id, contactB.id] });
+
+	contactA.say("抢答比赛").to(roomA);
+	var message = await waitForMessage(roomA);
+	assert.match(message.text(), /^抢答比赛即将进行/);
+
+	contactA.say("抢答比赛").to(roomB);
+	await assert.rejects(waitForMessage(roomB));
+
+	await bot.stop();
+});
+
 /**
  * @param {Contact | Room} conversation
  * @return {Promise<Message>}
